c8ece66af74ff109c5b14de449d1fc8836318689
K0ng2 <kongpop556677@gmail.com>
2022-05-25 12:24:36 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"github.com/K0ng2/bilisubdl/cmd"
 )
 
+var version string
+
 func main() {
+	cmd.RootCmd.Version = version
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatalln(err)
 	}
