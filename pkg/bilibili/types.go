7a408ef03695d25d5b200b51f0bdc204e75fbc7f
K0ng2 <kongpop556677@gmail.com>
2022-06-19 13:21:00 +0700

    remove unused tags

---
+++ pkg/bilibili/types.go
@@ -0,0 +1,40 @@
+package bilibili
+
+type Info struct {
+	Data    struct {
+		Season struct {
+			Title          string `json:"title"`
+		} `json:"season"`
+	} `json:"data"`
+}
+
+type Episodes struct {
+	Data    struct {
+		Sections []struct {
+			EpListTitle string `json:"ep_list_title"`
+			Episodes    []struct {
+				EpisodeID         int       `json:"episode_id"`
+				TitleDisplay      string    `json:"title_display"`
+			} `json:"episodes"`
+		} `json:"sections"`
+	} `json:"data"`
+}
+
+type Episode struct {
+	Data    struct {
+		Subtitles []struct {
+			URL        string `json:"url"`
+			Lang       string `json:"lang"`
+			LangKey    string `json:"lang_key"`
+		} `json:"subtitles"`
+	} `json:"data"`
+}
+
+type Subtitle struct {
+	Body            []struct {
+		From     float64 `json:"from"`
+		To       float64 `json:"to"`
+		Location int     `json:"location"`
+		Content  string  `json:"content"`
+	} `json:"body"`
+}
